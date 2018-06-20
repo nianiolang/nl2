c0ba84ddc66c70ecd43ecd2b5ead9b8af897d508
jjb <f.nl@atinea.pl>
2018-06-20 12:59:07 +0200

    nl2_201806201256_jjb_poprawienie_uprawnien_skryptow.patch

--- test_nls.sh
+++ test_nls.sh
@@ -3,7 +3,7 @@ set -exu
 
 rm -rf tmp
 mkdir tmp
-bash build_nls.sh --olympic_io --fe_lib
+./build_nls.sh --olympic_io --fe_lib
 mv nls tmp/
 
 echo "123" | tmp/nls/nls tmp/nls/ nls_nl.nl --  test/test_nls.nl test/test_nls.nl > tmp/out
